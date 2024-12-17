d94420d53b717979bce7aaa3ead5812a5f740fb8
CASTResearchLabs <CASTResearchLabs@users.noreply.github.com>
2024-12-17 08:42:06 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+SOURCES="${CARL_HOME}/sources"
+OUTPUT="${CARL_HOME}/output"
+SOURCES_WRK="${CARL_HOME}/sources_wrk"
+OUTPUT_WRK="${CARL_HOME}/output_wrk"
+LOGS="${OUTPUT_WRK}/Log/"
+CONF="${CARL_HOME}/configFile.xml"
+PROFILER="${CARL_HOME}/Profiler/profiler-results.json"
+
+main()
+{
+	mkdir -p ${SOURCES} ${OUTPUT} ${SOURCES_WRK} ${OUTPUT_WRK}
+	
+	cp -r ${SOURCES}/*  ${SOURCES_WRK}/
+
+	local options="-a ${APP} -s ${SOURCES_WRK} -o ${OUTPUT_WRK} -t ${LOGS}"
+	local tcc_options="-n ${APP} -i ${OUTPUT_WRK} -w ${OUTPUT_WRK}/Transactions -l ${LOGS}/Transactions.castlog"
+
+	if [ -f "${PROFILER}" ]; then
+		echo "[INFO] Profiler provided."
+		options+=" -d ${PROFILER}"
+	else
+		echo "[INFO] No profiler provided."
+	fi
+	if [ -f "${CONF}" ]; then
+		echo "[INFO] Configuration file provided."
+		options+=" -c ${CONF}"
+	else
+		echo "[INFO] No configuration file provided."
+	fi
+	echo "[INFO] Starting CARL."
+	carlengine ${options}
+
+
+	echo "[INFO] Starting CARL TCC."
+	carltcc ${tcc_options}
+
+	cp -r ${OUTPUT_WRK}/*  ${OUTPUT}/
+
+}
+
+main
